857b33caaf8c2fc8aeda5286fd0906ce2f963cb0
Tom Freischläger <tom@freischlaeger.de>
2025-05-15 11:22:04 +0200

    fix programm bug

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,15 +1,18 @@
 import * as Contentful from "contentful";
 
-export interface TypeKonzertFields {
-    projektnummer: Contentful.EntryFields.Integer;
-    titel?: Contentful.EntryFields.Symbol;
-    datumUhrzeit1: Contentful.EntryFields.Date;
-    datumUhrzeit2?: Contentful.EntryFields.Date;
-    programm?: Contentful.EntryFields.Symbol[];
-    solistinnenUndLeitung: Contentful.EntryFields.Symbol[];
-    ort: Contentful.EntryFields.Text;
-    eintrittspreise: Contentful.EntryFields.Text;
-    flyer?: Contentful.Asset;
+export interface KonzertFields {
+	projektnummer: Contentful.EntryFields.Integer;
+	titel?: Contentful.EntryFields.Symbol;
+	datumUhrzeit1: Contentful.EntryFields.Date;
+	datumUhrzeit2?: Contentful.EntryFields.Date;
+	programm?: Contentful.EntryFields.Symbol[];
+	solistinnenUndLeitung: Contentful.EntryFields.Symbol[];
+	ort: Contentful.EntryFields.Text;
+	eintrittspreise: Contentful.EntryFields.Text;
+	flyer?: Contentful.Asset;
 }
 
-export type TypeKonzert = Contentful.Entry<TypeKonzertFields>;
+export interface Konzert {
+	contentTypeId: "konzert";
+	fields: KonzertFields;
+}
